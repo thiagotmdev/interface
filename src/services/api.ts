7a9285e12e6179bfa0e400e25174c3414cd78e06
Thiago Mendes <thiagomendes.dev@gmail.com>
2025-09-05 13:11:25 -0300

    Enviando o token para o header

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,24 @@
-import axios, { type AxiosInstance } from "axios";
+import axios, { type AxiosInstance, type InternalAxiosRequestConfig } from "axios";
+import { firebaseAuth } from "../config/firebase";
 
 export const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000, //10 segundos
 });
+
+api.interceptors.request.use(
+  async (config: InternalAxiosRequestConfig): Promise<InternalAxiosRequestConfig> => {
+    const user = firebaseAuth.currentUser;
+
+    if (user) {
+      try {
+        const token = await user.getIdToken();
+        config.headers.set("Authorization", `Bearer ${token}`);
+      } catch (error) {
+        console.error("Erro ao obter token do usuário no Firebase", error);
+      }
+    }
+
+    return config;
+  },
+);
