6d6fd687dfb58faa97e43a21f1bb2bd1372e4993
Thiago Mendes <thiagomendes.dev@gmail.com>
2025-09-09 11:36:53 -0300

    Criado começo da tela de dashboard

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,7 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import MonthYearSelect from "../components/MonthYearSelect";
 import { api } from "../services/api";
 
 const DashBoard = () => {
+  const currentDate = new Date();
+  const [year, setYear] = useState<number>(currentDate.getFullYear());
+  const [month, setMonth] = useState(currentDate.getMonth() + 1);
+
   useEffect(() => {
     async function getTransactions() {
       const response = await api.get("/transactions");
@@ -11,6 +16,20 @@ const DashBoard = () => {
 
     getTransactions();
   }, []);
+
+  return (
+    <div className="container-app py-6">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6">
+        <h1 className="text-2xl font-bold mb-4 md:mb-0">Dashboard</h1>
+        <MonthYearSelect
+          month={month}
+          year={year}
+          onMonthChange={setMonth}
+          onYearChange={setYear}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default DashBoard;
