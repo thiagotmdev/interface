72c2460802460970517ffd3957a8b1eaf655d06d
Thiago Mendes <thiagomendes.dev@gmail.com>
2025-10-03 19:58:00 -0300

    Criando Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,99 @@
-import { Link } from "react-router";
+import { Activity, LogIn, LogOut } from "lucide-react";
+import { useState } from "react";
+import { Link, useLocation } from "react-router";
+import { useAuth } from "../context/AuthContext";
+
+interface NavLink {
+  name: string;
+  path: string;
+}
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const { authState, signOut } = useAuth();
+  const { pathname } = useLocation();
+
+  const isAuthenticated: boolean = !!authState.user; //Os dois !! faz com que o auth vire o boolean true pr false
+
+  const navLink: NavLink[] = [
+    { name: "Dashboard", path: "/dashboard" },
+    { name: "Transações", path: "/transacoes" },
+  ];
+
+  const handleSignOut = () => {
+    signOut();
+  };
+
+  const renderAvatar = () => {
+    if (!authState.user) return null;
+
+    if (authState.user.photoURL) {
+      <img
+        src={authState.user.photoURL}
+        alt={`authState.user.displayName`}
+        className="w-8 h-8 rounded-full border border-gray-700"
+      />;
+    }
+
+    return (
+      <div className="w-8 h-8 rounded-full bg-primary-500 flex items-center justify-center text-white font-medium">
+        {authState.user?.displayName?.charAt(0)}
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <h1>Header</h1>
-      <Link to="/transacoes">Transações</Link>
-    </div>
+    <header className="bg-gray-900 border-b border-gray-700">
+      <div className="container-app">
+        <div className="flex justify-between items-center py-4">
+          <Link to="/" className="flex gap-2 text-xl text-primary-500 items-center font-bold">
+            <Activity className="w-6 h-6" />
+            DevBills
+          </Link>
+
+          {isAuthenticated && (
+            <nav className="hidden md:flex space-x-3">
+              {navLink.map((link) => (
+                <Link
+                  key={link.name}
+                  to={link.path}
+                  className={
+                    pathname === link.path
+                      ? "text-primary-500 bg-primary-500/10 rounded-md h-10 px-3 py-2"
+                      : "text-gray-400 h-10 px-3 py-2 hover:text-primary-500 hover:bg-primary-500/5 rounded-md"
+                  }
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </nav>
+          )}
+
+          <div className="hidden md:flex items-center space-x-4">
+            {isAuthenticated ? (
+              <div className="flex items-center space-x-4">
+                <div className="flex items-center space-x-2">
+                  {renderAvatar()} <span>{authState.user?.displayName}</span>
+                </div>
+
+                <button
+                  type="button"
+                  onClick={handleSignOut}
+                  className="hover:text-red-300 hover:bg-red-500 p-2 rounded-full transition-colors cursor-pointer"
+                >
+                  <LogOut className="text-gray-200 " />
+                </button>
+              </div>
+            ) : (
+              <Link to="/login">
+                <LogIn className="bg-primary-500 text-gray-900 font-semibold px-5 py-2.5 rounded-xl flex items-center justify-center hover:bg-primary-500 transition-all" />
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
