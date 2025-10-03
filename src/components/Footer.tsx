ef4227673532fedb0bc7a8b93387fd5e3773c445
Thiago Mendes <thiagomendes.dev@gmail.com>
2025-10-03 16:09:53 -0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,15 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="bg-gray-800 border-t border-gray-700 py-4">
+      <div className="container-app">
+        <p className="text-sm text-gray-400 text-center">
+          DevBills {currentYear} - Desenvolvido por <strong>Thiago Mendes</strong> com {""}
+          <strong>TypeScript</strong> & <strong>React.JS</strong>
+        </p>
+      </div>
+    </footer>
   );
 };
 
